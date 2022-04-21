9e1e7e689a2d7035080390bc2c91d5ad9353227d
wxb <superwang9527@163.com>
2022-04-21 23:50:21 +0800

    feat: 初始化仓库

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import styles from './App.module.less'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className={styles.app}>
+
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8422
+  },
+  css: {
+    //* css模块化
+    modules: { // css模块化 文件以.module.[css|less|scss]结尾
+      generateScopedName: '[name]__[local]___[hash:base64:5]',
+      hashPrefix: 'prefix',
+    },
+    //* 预编译支持less
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true,
+      },
+    },
+  }
+})
